9bf85bb838c6a1a6c295adb6ea0f26ba3467dab7
BlakeB88 <bburnley6@gatech.edu>
2025-10-01 22:28:50 -0400

    Initial Commit

---
+++ StrainFitnessTracker/StrainFitnessTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrainFitnessTrackerApp.swift
+//  StrainFitnessTracker
+//
+//  Created by Blake Burnley on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct StrainFitnessTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StrainFitnessTrackerTests/StrainFitnessTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  StrainFitnessTrackerTests.swift
+//  StrainFitnessTrackerTests
+//
+//  Created by Blake Burnley on 10/1/25.
+//
+
+import Testing
+
+struct StrainFitnessTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
