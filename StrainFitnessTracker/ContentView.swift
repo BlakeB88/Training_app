0aa51d8c767159f09595ea18e1b520fb64fd022f
BlakeB88 <bburnley6@gatech.edu>
2025-10-08 20:14:50 -0400

    Update ContentView.swift

--- StrainFitnessTracker/ContentView.swift
+++ StrainFitnessTracker/ContentView.swift
@@ -80,7 +80,7 @@ struct ContentView: View {
             
             // Profile/Settings Tab
             NavigationStack {
-                AppSettingsView()
+                SettingsView()
             }
             .tabItem {
                 Label("Settings", systemImage: "gear")
